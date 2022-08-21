50037b1141b43b6148bc76e78ea4a637034e5ad2
fanchann <fannyarifnass@gmail.com>
2022-08-21 21:39:24 +0700

    21:30

---
+++ js/script.js
@@ -0,0 +1,88 @@
+const todoList = [
+    "Belajar JS Basic",
+    "Object",
+    "Belajar CSS"
+];
+
+function clearTodolist() {
+    const todoListBody = document.getElementById("todoListBody");
+    while (todoListBody.firstChild) {
+        todoListBody.removeChild(todoListBody.firstChild);
+    }
+}
+
+
+function removeTodoList(index) {
+    todoList.splice(index, 1);
+    displayTodolist()
+}
+
+function addTodoList(index, todo) {
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    tr.appendChild(td);
+
+    const buttonDone = document.createElement("input");
+    buttonDone.type = "button";
+    buttonDone.value = "Done";
+    buttonDone.onclick = function() {
+        removeTodoList(index)
+    }
+    td.appendChild(buttonDone)
+
+    const tdTodo = document.createElement("td");
+
+    let isi = tdTodo.textContent;
+    if (isi == 0) {
+        alert(`Isi tidak boleh kosong!`)
+    } else {
+        tdTodo.textContent = todo
+    }
+
+    tr.appendChild(tdTodo);
+
+    const todoListBody = document.getElementById("todoListBody");
+
+    todoListBody.appendChild(tr)
+
+}
+
+
+function displayTodolist() {
+    clearTodolist()
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todo = todoList[i];
+
+        const searchText = document.getElementById("search").value.toLocaleLowerCase();
+
+        if (todo.toLocaleLowerCase().includes(searchText)) {
+            addTodoList(i, todo)
+        }
+    }
+}
+
+
+document.forms['todoForm'].onsubmit = function(event) {
+    event.preventDefault();
+
+    const todo = document.forms['todoForm']['todo'].value;
+    todoList.push(todo);
+
+    document.forms['todoForm'].reset();
+
+    displayTodolist()
+};
+
+const search = document.getElementById("search");
+
+
+search.onkeyup = function() {
+    displayTodolist()
+}
+
+search.onkeydown = function() {
+    displayTodolist()
+}
+
+displayTodolist()
